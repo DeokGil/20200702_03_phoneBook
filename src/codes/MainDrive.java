69fc976dccd7d03d902aa90b098d0bbd46a48bed
DeokGil <67721179+DeokGil@users.noreply.github.com>
2020-07-02 12:55:25 +0900

    1번 선택시 연락처 정보 입력받기

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -3,61 +3,75 @@ package codes;
 import java.util.Scanner;
 
 public class MainDrive {
-	
+
 	public static void main(String[] args) {
-		
+
 //		Git과 연동해서 전화번호부 만들 예정
-		
+
 		printMenu();
-		
+
 	}
-	
+
 	public static void printMenu() {
-		
+
 //		메뉴 1, 2, 0번 => 0번을 누르면 프로그램 종료.
 //		0번을 누를떄까지는 무한 반복.
-		
+
 		Scanner myScan = new Scanner(System.in);
-		
-		while(true) {
-			
+
+		while (true) {
+
 //			어떤 메뉴가 있는지 표기
 			System.out.println("*******전화번호부*******");
 			System.out.println("1) 전화번호 추가 등록");
 			System.out.println("2) 전체 전화번호 목록 조회");
 			System.out.println("0) 프로그램종료");
 			System.out.println("=====================");
-			
+
 //			실제 메뉴 입력받기
 			System.out.print("메뉴 선택 : ");
 			int inputMenu = myScan.nextInt();
-			
+
 //			입력값 확인
-			if(inputMenu == 0) {
+			if (inputMenu == 0) {
 //				프로그램 종료 => 무한 반복 탈출.
 				System.out.println("전화번호부를 종료합니다.");
 				break;
-			}
-			else if(inputMenu ==1) {
-//				전화번호부 추가 기능 구현 필요
-			}
-			else if(inputMenu ==2) {
+			} else if (inputMenu == 1) {
+//				전화번호 등록기능 실행
+				addPhoneNumToFile();
+			} else if (inputMenu == 2) {
 //				전화번호 전체 조회 기능 구현 필요
-			}
-			else {
+			} else {
 //				0,1,2 외의 값이 들어온 경우 대응.
 				System.out.println("잘못된 입력입니다.");
 				System.out.println("다시 입력해주세요.");
 			}
-			
+
 		}
+
+	}
+	
+//	전화번호 + 이름 + 생년 정보 저장 기능.
+	public static void addPhoneNumToFile() {
+	
+//		저장할 데이터를 입력받자.
+		Scanner myScan = new Scanner(System.in);
 		
-	
-	
-	
-	
-	
-	
+//		이름(String) -> 폰번(String) -> 생년(int) 순서대로 저장
+		
+		System.out.print("이름 입력 : ");
+		String name = myScan.nextLine();
+		
+		System.out.print("전화번호 입력 : ");
+		String phoneNum = myScan.nextLine();
+		
+		System.out.print("생년 입력 : ");
+		int birthYear = myScan.nextInt();
+		
+//		변수에 저장한 데이터를 묶어서 파일로 저장.
+//		JAVA => 보조기억장치 내보내기 => 파일출력(SAVE)
+		
 	}
 
 }
